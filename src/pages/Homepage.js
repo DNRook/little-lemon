fcc83b61ac0b515b7f997f086f45088e42bc004f
Your Name <you@example.com>
2023-04-23 17:15:08 -0400

    homepage

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,28 +1,103 @@
 import React from 'react'
-
+import bruchetta from '../Assets/bruchetta.png';
+import greek from '../Assets/greek_salad.jpg';
+import lemon from '../Assets/lemon_dessert.jpg';
+import { Link} from "react-router-dom";
 const Homepage = () => {
   return (
     <>
       <section className='homepage_section'>
-        <div>
+        <section className='homepage-header'>
           <h2>Specials</h2>
-          <button>Order Now</button>
-        </div>
-        <section>
-          <div>
-            card
+          <Link to="/booking">
+            <button className='order-button'>Order Now</button>
+          </Link>
+          
+        </section>
+        <section className='card-grid'>
+          <div className='card'>
+            <div className='card-container'>
+              <img src={bruchetta} alt="bruchetta card image" role="image" className='card-image'/>
+              <p className='item'>Bruchetta <span className='item-price'>$5.99</span></p>
+              <p className='desc'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                 Ut enim ad minim veniam.
+              </p>
+              <p className='card-bottom'>Order Delivery <span className='delivery-price'>$1</span></p>
+            </div>
           </div>
-          <div>
-            card
+          <div className='card'>
+            <div className='card-container'>
+              <img src={greek} alt="bruchetta card image" role="image" className='card-image'/>
+              <p className='item'>Greek Salad <span className='item-price'>$5.99</span></p>
+              <p className='desc'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                 Ut enim ad minim veniam.
+              </p>
+              <p className='card-bottom'>Order Delivery <span className='delivery-price'>$1</span></p>
+            </div>
           </div>
-          <div>
-            card
+          <div className='card'>
+            <div className='card-container'>
+              <img src={lemon} alt="bruchetta card image" role="image" className='card-image'/>
+              <p className='item'>Lemon dessert <span className='item-price'>$5.99</span></p>
+              <p className='desc'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                 Ut enim ad minim veniam.
+              </p>
+              <p className='card-bottom'>Order Delivery <span className='delivery-price'>$1</span></p>
+            </div>
           </div>
         </section>
-        <section>
-          testimonials
+     
+        <section className='testimonial'>
+          <h1>Testimonial</h1>
+          <div className='testimonal-container'>  
+          <div className='testimonial-card'>
+            <div>
+              <h4>user</h4>  
+              <p className='desc'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+              <span class="fa fa-star checked"></span>
+              <span class="fa fa-star checked"></span>
+              <span class="fa fa-star checked"></span>
+              <span class="fa fa-star"></span>
+              <span class="fa fa-star"></span>
+            </div>
+          </div>
+          <div className='testimonial-card'>
+            <div>
+              <h4>user</h4>  
+              <p className='desc'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+              <span class="fa fa-star checked"></span>
+              <span class="fa fa-star checked"></span>
+              <span class="fa fa-star checked"></span>
+              <span class="fa fa-star"></span>
+              <span class="fa fa-star"></span>
+            </div>
+          </div>
+          <div className='testimonial-card'>
+            <div>
+              <h4>user</h4>  
+              <p className='desc'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+              <span class="fa fa-star checked"></span>
+              <span class="fa fa-star checked"></span>
+              <span class="fa fa-star checked"></span>
+              <span class="fa fa-star"></span>
+              <span class="fa fa-star"></span>
+            </div>
+          </div>
+          <div className='testimonial-card'>
+            <div>
+              <h4>user</h4>  
+              <p className='desc'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+              <span class="fa fa-star checked"></span>
+              <span class="fa fa-star checked"></span>
+              <span class="fa fa-star checked"></span>
+              <span class="fa fa-star"></span>
+              <span class="fa fa-star"></span>
+            </div>
+          </div>
+        </div>
         </section>
       </section>
+  
     </>
   )
 }
